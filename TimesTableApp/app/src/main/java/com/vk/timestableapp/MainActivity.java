563dd38d237517ebc0b8064783bef1ec04e61ead
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2019-02-13 13:33:55 +0530

    times table app code initialized
    the main code and the xmls are initialized and completed

--- TimesTableApp/app/src/main/java/com/vk/timestableapp/MainActivity.java
+++ TimesTableApp/app/src/main/java/com/vk/timestableapp/MainActivity.java
@@ -1,13 +1,56 @@
 package com.vk.timestableapp;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+
+    public void generateTable(int progress){
+        final ArrayList<String> tableArray =  new ArrayList<String>();
+        int x = 1;
+        while(x<=10){
+            tableArray.add(Integer.toString(x*progress));
+            x++;
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tableArray);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        listView = (ListView) findViewById(R.id.listView);
+        seekBar.setMax(20);
+        seekBar.setMin(2);
+        seekBar.setProgress(2);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+               generateTable(progress);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
