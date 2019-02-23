0136a11b78ab3b0e3ab1e40d642f2f77bff083f2
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2019-02-23 22:17:37 +0530

    Downloading Image frame app created

---
+++ DownloadingImagesDemo/app/src/main/java/com/vk/downloadingimagesdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.vk.downloadingimagesdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void downloadImage (View view){
+        Log.i("Button Tapped","OK");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
