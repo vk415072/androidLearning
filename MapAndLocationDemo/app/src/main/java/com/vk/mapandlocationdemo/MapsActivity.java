9d76ec069de9a920b30f972659d8cefa72f82db9
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2019-03-15 13:10:23 +0530

    Finalized!
    learnt to intigrate user's current location with google map.
    This app will now show the user's current location on map and keep it up to date (live location).

--- MapAndLocationDemo/app/src/main/java/com/vk/mapandlocationdemo/MapsActivity.java
+++ MapAndLocationDemo/app/src/main/java/com/vk/mapandlocationdemo/MapsActivity.java
@@ -1,10 +1,22 @@
 package com.vk.mapandlocationdemo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -13,6 +25,28 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+        //after the permission has been granted, this method would executed.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            //checking thr request code.
+        if(requestCode == 1) {
+                //checking if we've the permission.
+                //if both conditions are true then we have the permission.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //checking again, if we do have the permission.
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        //requesting location updates.
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +69,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(final GoogleMap googleMap) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //GETTING USER'S LOCATION.
+        //accessing the location services of the phone.
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("hehehe", location.toString());
+                mMap = googleMap;
+                    //to clear the previous markers.
+                mMap.clear();
+                    // initiating a LatLang type variable which holds the latitude and longitude of a location.
+                    //here we are passing the location, converting it to latitude & longitude.
+                LatLng usersLatLang = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(usersLatLang).title("My Location"));
+                    //moving the camera to the user's location, also zooming to 15.
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usersLatLang,16));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+            //in modern android we need to ask the user for the specific permission (Location Services in our case).
+            //checking if we don't have permission.
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                //asking for permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                //if the permission is granted then onRequestPermissionResult() method would execute.
+
+            //if we already has permission then onRequestPermissionsResult() would not execute which contains the location requesting command so,
+            //we've to implement an else statement too.
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                //when we launch the app, it does not show any marker until we change the position or
+                //the marker will only pins whenever we change the location.
+                //to deal with this we are appointing a marker to the user's last known location.
+            //this is not working...
+            /*Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                //applying the same for making a marker.
+            LatLng usersLatLang = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(usersLatLang).title("My Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(usersLatLang));
+            */
+        }
     }
+
 }
