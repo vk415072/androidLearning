0739789402bac85b5f3b0b6cd5ba4c30988fc548
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2019-03-15 11:19:31 +0530

    Finalized
    added comments for the help.
    Learnt to get user's location and ask for locatio permission.

--- LocationDemoApp/app/src/main/java/com/vk/locationdemoapp/MainActivity.java
+++ LocationDemoApp/app/src/main/java/com/vk/locationdemoapp/MainActivity.java
@@ -6,43 +6,72 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+        //after the permission granted this will exicuted.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            //if both conditions are true then we have the permission.
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //checking again, if we do have permission.
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //requesting the location updates.
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
+            //allow to access the location service of the phone.
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            //LocationListener will listen for changes in the location.
+        locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                Log.i("Location", location.toString());
+                    //logging the location if it changes.
+                Log.i("Location",location.toString());
+                    //also updating textView as per the location changes.
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(location.toString());
             }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
+            public void onStatusChanged(String s, int i, Bundle bundle) {
             }
 
             @Override
-            public void onProviderEnabled(String provider) {
-
+            public void onProviderEnabled(String s) {
             }
 
             @Override
-            public void onProviderDisabled(String provider) {
-
+            public void onProviderDisabled(String s) {
             }
         };
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            //in modern android we need to ask the user for the specific permission (Location Services in our case).
+            //checking if we don't have permission.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //asking permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+                //if we already has permission then onRequestPermissionsResult() would not execute which contains the location requesting command so,
+                //we've to implement an else statement too.
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
     }
 }
